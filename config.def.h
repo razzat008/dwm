780c8fbc00956c3ab9a2d98581e8ee7c4c40a9e0
ceaser008 <radiusrazzat111@gmail.com>
2023-03-02 22:06:30 +0545

    added fullgaps patch

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx = 0; /* border pixel of windows */
-static const unsigned int gappx = 9;    /* gap pixel between windows */
+static const unsigned int gappx = 5;    /* gap pixel between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
@@ -153,6 +153,10 @@ static const Key keys[] = {
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
 // switch between master and slave stack
+  //
+  	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+  	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+  	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
     {MODKEY | ShiftMask, XK_Return, zoom, {0}},
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY, XK_q, killclient, {0}},
